24def13626cebb6e8eeb987ee4bda1628de493ed
gufi <carol.domokos@gmail.com>
2016-04-06 09:11:28 +0200

    relative path changes

--- OSX/osx.py
+++ OSX/osx.py
@@ -13,8 +13,9 @@ from pprint import pprint
 #osxDict = {'confiuredReleaseNr': '10.11.3'}
 #with open('osx.json', 'w') as output:
 #     json.dump(osxDict, output)
-json_data=open('/home/gufi/PYTHON_Learning/osx.json').read()
-with open('/home/gufi/PYTHON_Learning/osx.json') as data_file:
+dir_path = os.path.dirname(os.path.abspath(__file__))
+json_data=open(os.path.join(dir_path, 'osx.json')).read()
+with open(os.path.join(dir_path, 'osx.json')) as data_file:
      data = json.load(data_file)
 pprint("Staticaly configured release value: "+str(data['confiuredReleaseNr']))
 # downlod the OSX wiki page and create the BS4 types
@@ -44,14 +45,14 @@ tableCellSmall = osxSoup.select('table.infobox.vevent small')
 daysAgo = ''.join( bs4.BeautifulSoup(tableCellSmall[0].text, "lxml").findAll(text=True))
 ### screen output if run from cli
 print("Latest release nr: "+str(latestReleaseNr))
-print("Release date: "+daysAgo)
+#print("Release date: "+daysAgo)
 # if the value from infobox matches ours no action is needed just send a reassuring email
 configuredReleaseNr = data['confiuredReleaseNr']
 if latestReleaseNr == configuredReleaseNr:
 	# convert latest releas nr to string so we can add to file
 	latestReleaseNrStr = str(latestReleaseNr)
 	print 'No change needed!'
-	noChangeNeededFile = codecs.open('/home/gufi/PYTHON_Learning/noChangeNeeded.txt', encoding='utf-8', mode= 'w+')
+	noChangeNeededFile = codecs.open(os.path.join(dir_path, 'noChangeNeeded.txt'), encoding='utf-8', mode= 'w+')
 	noChangeNeededFile.write('No change needed : \n')
 	noChangeNeededFile.write('The current release number is: ')
 	noChangeNeededFile.write(latestReleaseNrStr)
@@ -60,7 +61,7 @@ if latestReleaseNr == configuredReleaseNr:
 	noChangeNeededFile.write('\n Release date: ')
 	noChangeNeededFile.write(daysAgo)
 	noChangeNeededFile.close()
-	fp = open('/home/gufi/PYTHON_Learning/noChangeNeeded.txt', 'rb')
+	fp = open(os.path.join(dir_path, 'noChangeNeeded.txt'), 'rb')
 	msg = MIMEText(fp.read())
 	fp.close()
 	msg['From'] = 'carol.domokos@gmail.com'
@@ -69,7 +70,7 @@ if latestReleaseNr == configuredReleaseNr:
 	smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 	smtpObj.ehlo()
  	smtpObj.starttls()
-	smtpObj.login('carol.domokos@gmail.com','xxxx')
+	smtpObj.login('carol.domokos@gmail.com','xxxxx')
 	smtpObj.sendmail('carol.domokos@gmail.com', ['carol.domokos@hp.com'], msg.as_string())
 	smtpObj.quit()
 else:
@@ -81,7 +82,8 @@ else:
 	print 'Change needed!!!!!'
         # convert latest releas nr to string so we can add to file
         latestReleaseNrStr = str(latestReleaseNr)
-        ChangeNeededFile = codecs.open('/home/gufi/PYTHON_Learning/ChangeNeeded.txt', encoding='utf-8', mode= 'w+')
+        ChangeNeededFile = codecs.open(os.path.join(dir_path, 'ChangeNeeded.txt'), encoding='utf-8', mode= 'w+')
+        ChangeNeededFile.write('A change is needed!!!!! \n')
         ChangeNeededFile.write('The current release number is: \n')
        	ChangeNeededFile.write(latestReleaseNrStr)
        	ChangeNeededFile.write('\n The staticaly configured release value is: ')
@@ -89,16 +91,16 @@ else:
        	ChangeNeededFile.write('\n Release date: ')
        	ChangeNeededFile.write(daysAgo)
        	ChangeNeededFile.close()
-        fp = open('/home/gufi/PYTHON_Learning/ChangeNeeded.txt', 'rb')
+        fp = open(os.path.join(dir_path, 'ChangeNeeded.txt'), 'rb')
         msg = MIMEText(fp.read())
         fp.close()
        	msg['From'] = 'carol.domokos@gmail.com'
         msg['To'] = 'carol.domokos@hpe.com'
-       	msg['Subject'] = 'OSX latest release'
+       	msg['Subject'] = 'OSX latest release - CHANGE NEEDED'
         smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
         smtpObj.ehlo()
         smtpObj.starttls()
-       	smtpObj.login('carol.domokos@gmail.com','xxxxx')
+       	smtpObj.login('carol.domokos@gmail.com','xxxx')
         smtpObj.sendmail('carol.domokos@gmail.com', ['carol.domokos@hp.com'], msg.as_string())
        	smtpObj.quit()
 
